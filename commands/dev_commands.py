8fca79a340c496d87ca6e5bd2f514d1a8409015e
jefffffc <jeffchoy94610@gmail.com>
2017-10-23 13:12:02 +0800

    fix updatelang

--- commands/dev_commands.py
+++ commands/dev_commands.py
@@ -2,7 +2,7 @@ from language import user_locale
 from utils import dev_only
 from telegram.parsemode import ParseMode
 from telegram.ext import CommandHandler
-from telegram.utils.request import Request
+from telegram.vendor.ptb_urllib3.urllib3 import PoolManager
 from config import POEDITOR_WEBHOOK
 from shared_vars import dispatcher
 
@@ -10,9 +10,9 @@ from shared_vars import dispatcher
 @user_locale
 @dev_only
 def update_language_callback(bot, update):
-    r = Request()
-    result = r.get(POEDITOR_WEBHOOK)
-    update.message.reply_rext("`{}`".format(result), parse_mode=ParseMode.MARKDOWN)
+    r = PoolManager()
+    result = r.request('GET', POEDITOR_WEBHOOK)
+    update.message.reply_rext("`{}`".format(result.reason), parse_mode=ParseMode.MARKDOWN)
     return
 
 
